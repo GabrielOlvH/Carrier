fcfe68a3ca673a5c3628751cd200837d47e1de42
StupPlayer <gabrielh.oliveira222@gmail.com>
2020-09-16 17:55:40 -0300

    Cancel hotbar scroll when carrying

---
+++ src/main/java/me/steven/carrier/mixin/MixinPlayerInventory.java
@@ -0,0 +1,28 @@
+package me.steven.carrier.mixin;
+
+import me.steven.carrier.api.Holder;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.player.PlayerInventory;
+import net.minecraft.network.packet.c2s.play.UpdateSelectedSlotC2SPacket;
+import net.minecraft.server.network.ServerPlayNetworkHandler;
+import net.minecraft.server.network.ServerPlayerEntity;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(PlayerInventory.class)
+public class MixinPlayerInventory {
+
+    @Shadow @Final public PlayerEntity player;
+
+    @Inject(method = "scrollInHotbar", at = @At("HEAD"), cancellable = true)
+    private void carrier_cancelSelectedSlotChange(double scrollAmount, CallbackInfo ci) {
+        if (player instanceof Holder) {
+            Holder holder = (Holder) player;
+            if (holder.getHolding() != null) ci.cancel();
+        }
+    }
+}
