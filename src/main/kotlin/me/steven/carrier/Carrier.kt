dcb62130a32bded29200a875ee491b906a137581
StupPlayer <gabrielh.oliveira222@gmail.com>
2020-09-15 21:32:59 -0300

    Configure project

---
+++ src/main/kotlin/me/steven/carrier/Carrier.kt
@@ -0,0 +1,11 @@
+package me.steven.carrier
+
+import net.fabricmc.api.ModInitializer
+
+object Carrier : ModInitializer {
+    override fun onInitialize() {
+        
+    }
+
+    const val MOD_ID = "carrier"
+}
