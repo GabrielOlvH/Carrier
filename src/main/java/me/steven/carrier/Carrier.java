2d8c3b8681efad6ffd200f799286a3c21cb490d6
StupPlayer <gabrielh.oliveira222@gmail.com>
2020-09-19 18:56:36 -0300

    Removed unused stuff

--- src/main/java/me/steven/carrier/Carrier.java
+++ src/main/java/me/steven/carrier/Carrier.java
@@ -4,30 +4,21 @@ import dev.onyxstudios.cca.api.v3.component.ComponentKey;
 import dev.onyxstudios.cca.api.v3.component.ComponentRegistryV3;
 import dev.onyxstudios.cca.api.v3.entity.EntityComponentFactoryRegistry;
 import dev.onyxstudios.cca.api.v3.entity.EntityComponentInitializer;
-import me.steven.carrier.api.*;
+import me.steven.carrier.api.CarriableRegistry;
+import me.steven.carrier.api.Holder;
 import me.steven.carrier.impl.*;
 import nerdhub.cardinal.components.api.util.RespawnCopyStrategy;
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents;
 import net.fabricmc.fabric.api.event.player.UseBlockCallback;
 import net.fabricmc.fabric.api.event.player.UseEntityCallback;
-import net.minecraft.block.Block;
-import net.minecraft.block.Blocks;
-import net.minecraft.util.ActionResult;
 import net.minecraft.util.Identifier;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 
 public class Carrier implements ModInitializer, EntityComponentInitializer {
 
     public static final ComponentKey<Holder> HOLDER = ComponentRegistryV3.INSTANCE.getOrCreate(new Identifier("carrier", "holder"), Holder.class);
 
     public static final String MOD_ID = "carrier";
-
-    public static final Logger LOGGER = LogManager.getLogger("Carrier");
-
-    public static final Identifier SYNC_CARRYING_PACKET = new Identifier("carrier", "sync_carrying");
 
     @Override
     public void onInitialize() {
