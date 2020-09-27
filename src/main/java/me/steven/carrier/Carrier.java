b42624788d5f6933a2b3e73197a224baf07940d5
StupPlayer <gabrielh.oliveira222@gmail.com>
2020-09-27 19:33:30 -0300

    Allow clients to have their own configs

--- src/main/java/me/steven/carrier/Carrier.java
+++ src/main/java/me/steven/carrier/Carrier.java
@@ -6,6 +6,7 @@ import dev.onyxstudios.cca.api.v3.component.ComponentKey;
 import dev.onyxstudios.cca.api.v3.component.ComponentRegistryV3;
 import dev.onyxstudios.cca.api.v3.entity.EntityComponentFactoryRegistry;
 import dev.onyxstudios.cca.api.v3.entity.EntityComponentInitializer;
+import me.steven.carrier.api.Carriable;
 import me.steven.carrier.api.CarriableRegistry;
 import me.steven.carrier.api.Holder;
 import me.steven.carrier.impl.*;
@@ -15,12 +16,16 @@ import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents;
 import net.fabricmc.fabric.api.event.player.UseBlockCallback;
 import net.fabricmc.fabric.api.event.player.UseEntityCallback;
 import net.fabricmc.fabric.api.event.registry.RegistryEntryAddedCallback;
+import net.fabricmc.fabric.api.network.ServerSidePacketRegistry;
 import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.block.AbstractChestBlock;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockEntityProvider;
+import net.minecraft.entity.Entity;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.registry.Registry;
+import net.minecraft.world.World;
 import org.apache.commons.io.FileUtils;
 import org.apache.logging.log4j.LogManager;
 
@@ -36,6 +41,9 @@ public class Carrier implements ModInitializer, EntityComponentInitializer {
     public static final String MOD_ID = "carrier";
 
     public static Config CONFIG = new Config();
+    
+    public static final Identifier C2S_CARRY_BLOCK_PACKET = new Identifier(MOD_ID, "carry_block_packet");
+    public static final Identifier C2S_CARRY_ENTITY_PACKET = new Identifier(MOD_ID, "carry_entity_packet");
 
     @Override
     public void onInitialize() {
@@ -62,16 +70,16 @@ public class Carrier implements ModInitializer, EntityComponentInitializer {
         ServerTickEvents.END_WORLD_TICK.register(new ServerWorldTickCallback());
         UseBlockCallback.EVENT.register(HolderInteractCallback.INSTANCE);
         UseEntityCallback.EVENT.register(HolderInteractCallback.INSTANCE);
-        CarriableRegistry.INSTANCE.register(new Identifier("carrier", "cow"), new CarriableCow());
-        CarriableRegistry.INSTANCE.register(new Identifier("carrier", "chicken"), new CarriableChicken());
-        CarriableRegistry.INSTANCE.register(new Identifier("carrier", "parrot"), new CarriableParrot());
-        CarriableRegistry.INSTANCE.register(new Identifier("carrier", "pig"), new CarriablePig());
-        CarriableRegistry.INSTANCE.register(new Identifier("carrier", "rabbit"), new CarriableRabbit());
-        CarriableRegistry.INSTANCE.register(new Identifier("carrier", "sheep"), new CarriableSheep());
-        CarriableRegistry.INSTANCE.register(new Identifier("carrier", "turtle"), new CarriableTurtle());
-        CarriableRegistry.INSTANCE.register(new Identifier("carrier", "wolf"), new CarriableWolf());
-        CarriableRegistry.INSTANCE.register(new Identifier("carrier", "spawner"), new CarriableSpawner(new Identifier("carrier", "spawner")));
-        CarriableRegistry.INSTANCE.register(new Identifier("carrier", "enchanting_table"), new CarriableEnchantingTable(new Identifier("carrier", "enchanting_table")));
+        CarriableRegistry.INSTANCE.register(new Identifier(MOD_ID, "cow"), new CarriableCow());
+        CarriableRegistry.INSTANCE.register(new Identifier(MOD_ID, "chicken"), new CarriableChicken());
+        CarriableRegistry.INSTANCE.register(new Identifier(MOD_ID, "parrot"), new CarriableParrot());
+        CarriableRegistry.INSTANCE.register(new Identifier(MOD_ID, "pig"), new CarriablePig());
+        CarriableRegistry.INSTANCE.register(new Identifier(MOD_ID, "rabbit"), new CarriableRabbit());
+        CarriableRegistry.INSTANCE.register(new Identifier(MOD_ID, "sheep"), new CarriableSheep());
+        CarriableRegistry.INSTANCE.register(new Identifier(MOD_ID, "turtle"), new CarriableTurtle());
+        CarriableRegistry.INSTANCE.register(new Identifier(MOD_ID, "wolf"), new CarriableWolf());
+        CarriableRegistry.INSTANCE.register(new Identifier(MOD_ID, "spawner"), new CarriableSpawner(new Identifier(MOD_ID, "spawner")));
+        CarriableRegistry.INSTANCE.register(new Identifier(MOD_ID, "enchanting_table"), new CarriableEnchantingTable(new Identifier(MOD_ID, "enchanting_table")));
 
         Registry.BLOCK.forEach((block) -> {
             Identifier type = new Identifier("carrier", Registry.BLOCK.getId(block).getPath());
@@ -81,6 +89,33 @@ public class Carrier implements ModInitializer, EntityComponentInitializer {
             Identifier type = new Identifier("carrier", id.getPath());
             registerGenericCarriable(block, type);
         });
+
+        ServerSidePacketRegistry.INSTANCE.register(C2S_CARRY_BLOCK_PACKET, (ctx, buf) -> {
+            BlockPos pos = buf.readBlockPos();
+            Holder holder = HOLDER.get(ctx.getPlayer());
+            ctx.getTaskQueue().execute(() -> {
+                World world = ctx.getPlayer().world;
+                Block block = world.getBlockState(pos).getBlock();
+                Carriable<?> carriable = CarriableRegistry.INSTANCE.get(block);
+                if (world.canPlayerModifyAt(ctx.getPlayer(), pos) && carriable != null && canCarry(Registry.BLOCK.getId(block)))
+                    carriable.tryPickup(holder, world, pos, null);
+            });
+        });
+
+        ServerSidePacketRegistry.INSTANCE.register(C2S_CARRY_ENTITY_PACKET, (ctx, buf) -> {
+            int entityId = buf.readInt();
+            ctx.getTaskQueue().execute(() -> {
+                World world = ctx.getPlayer().world;
+                Entity entity = world.getEntityById(entityId);
+                if (entity != null) {
+                    Holder holder = HOLDER.get(ctx.getPlayer());
+                    BlockPos pos = entity.getBlockPos();
+                    Carriable<?> carriable = CarriableRegistry.INSTANCE.get(entity.getType());
+                    if (world.canPlayerModifyAt(ctx.getPlayer(), pos) && carriable != null && canCarry(Registry.ENTITY_TYPE.getId(entity.getType())))
+                        carriable.tryPickup(holder, world, pos, entity);
+                }
+            });
+        });
     }
 
     @Override
@@ -88,7 +123,7 @@ public class Carrier implements ModInitializer, EntityComponentInitializer {
         registry.registerForPlayers(HOLDER, Holder::new, RespawnCopyStrategy.ALWAYS_COPY);
     }
 
-    public static boolean canRegister(Identifier id) {
+    public static boolean canCarry(Identifier id) {
         if (CONFIG.getType() == Config.ListType.WHITELIST) return CONFIG.getList().stream().anyMatch((s) -> Pattern.compile(s).matcher(id.toString()).find());
         else return CONFIG.getList().stream().noneMatch((s) -> Pattern.compile(s).matcher(id.toString()).find());
     }
--- src/main/java/me/steven/carrier/HolderInteractCallback.java
+++ src/main/java/me/steven/carrier/HolderInteractCallback.java
@@ -1,11 +1,14 @@
 package me.steven.carrier;
 
+import io.netty.buffer.Unpooled;
 import me.steven.carrier.api.*;
 import net.fabricmc.fabric.api.event.player.UseBlockCallback;
 import net.fabricmc.fabric.api.event.player.UseEntityCallback;
+import net.fabricmc.fabric.api.network.ClientSidePacketRegistry;
 import net.minecraft.block.Block;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.network.PacketByteBuf;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
 import net.minecraft.util.hit.BlockHitResult;
@@ -18,49 +21,53 @@ public class HolderInteractCallback implements UseBlockCallback, UseEntityCallba
 
     public static final HolderInteractCallback INSTANCE = new HolderInteractCallback();
 
-    private HolderInteractCallback() {}
+    private HolderInteractCallback() {
+    }
 
     @Override
     public ActionResult interact(PlayerEntity player, World world, Hand hand, BlockHitResult hitResult) {
-        if (world.isClient || hand == Hand.OFF_HAND) return ActionResult.PASS;
+        if (hand == Hand.OFF_HAND) return ActionResult.PASS;
         BlockPos pos = hitResult.getBlockPos();
+        if (!world.canPlayerModifyAt(player, pos)) return ActionResult.PASS;
         Block block = world.getBlockState(pos).getBlock();
         Holder holder = Carrier.HOLDER.get(player);
-            Holding holding = holder.getHolding();
-            if (holding == null && player.isSneaking() && CarriableRegistry.INSTANCE.contains(block) && player.getStackInHand(hand).isEmpty() && Carrier.canRegister(Registry.BLOCK.getId(block))) {
-                Carriable<?> carriable = CarriableRegistry.INSTANCE.get(block);
-                ActionResult actionResult = carriable.tryPickup(holder, world, pos, null);
-                if (actionResult.isAccepted())
-                    return actionResult;
-            }
-            if (holding == null) return ActionResult.PASS;
+        Holding holding = holder.getHolding();
+        if (world.isClient && holding == null && player.isSneaking() && CarriableRegistry.INSTANCE.contains(block) && player.getStackInHand(hand).isEmpty() && Carrier.canCarry(Registry.BLOCK.getId(block))) {
+            PacketByteBuf buf = new PacketByteBuf(Unpooled.buffer());
+            buf.writeBlockPos(hitResult.getBlockPos());
+            ClientSidePacketRegistry.INSTANCE.sendToServer(Carrier.C2S_CARRY_BLOCK_PACKET, buf);
+            return ActionResult.CONSUME;
+        }
+
+        if (holding != null) {
             Carriable<?> carriable = CarriableRegistry.INSTANCE.get(holding.getType());
-            if (carriable != null) {
+            if (!world.isClient && carriable != null) {
                 ActionResult actionResult = carriable.tryPlace(holder, world, new CarriablePlacementContext(holder, carriable, pos.offset(hitResult.getSide()), hitResult.getSide(), player.getHorizontalFacing()));
                 if (actionResult.isAccepted()) return actionResult;
             }
+        }
 
         return ActionResult.PASS;
     }
 
     @Override
     public ActionResult interact(PlayerEntity player, World world, Hand hand, Entity entity, EntityHitResult hitResult) {
-        if (world.isClient || hand == Hand.OFF_HAND) return ActionResult.PASS;
+        if (hand == Hand.OFF_HAND || !world.canPlayerModifyAt(player, entity.getBlockPos())) return ActionResult.PASS;
         BlockPos pos = entity.getBlockPos();
         Holder holder = Carrier.HOLDER.get(player);
-            Holding holding = holder.getHolding();
-            if (holding == null && player.isSneaking() && CarriableRegistry.INSTANCE.contains(entity.getType()) && player.getStackInHand(hand).isEmpty() && Carrier.canRegister(Registry.ENTITY_TYPE.getId(entity.getType()))) {
-                Carriable<?> carriable = CarriableRegistry.INSTANCE.get(entity.getType());
-                ActionResult actionResult = carriable.tryPickup(holder, world, pos, entity);
-                if (actionResult.isAccepted())
-                    return actionResult;
-            }
-            if (holding == null) return ActionResult.PASS;
-            Carriable<?> carriable = CarriableRegistry.INSTANCE.get(holding.getType());
-            if (carriable != null) {
-                ActionResult actionResult = carriable.tryPlace(holder, world, new CarriablePlacementContext(holder, carriable, pos, player.getHorizontalFacing(), player.getHorizontalFacing()));
-                if (actionResult.isAccepted()) return actionResult;
-            }
+        Holding holding = holder.getHolding();
+        if (world.isClient && holding == null && player.isSneaking() && CarriableRegistry.INSTANCE.contains(entity.getType()) && player.getStackInHand(hand).isEmpty() && Carrier.canCarry(Registry.ENTITY_TYPE.getId(entity.getType()))) {
+            PacketByteBuf buf = new PacketByteBuf(Unpooled.buffer());
+            buf.writeInt(entity.getEntityId());
+            ClientSidePacketRegistry.INSTANCE.sendToServer(Carrier.C2S_CARRY_ENTITY_PACKET, buf);
+            return ActionResult.CONSUME;
+        }
+        if (holding == null) return ActionResult.PASS;
+        Carriable<?> carriable = CarriableRegistry.INSTANCE.get(holding.getType());
+        if (!world.isClient && carriable != null) {
+            ActionResult actionResult = carriable.tryPlace(holder, world, new CarriablePlacementContext(holder, carriable, pos, player.getHorizontalFacing(), player.getHorizontalFacing()));
+            if (actionResult.isAccepted()) return actionResult;
+        }
         return ActionResult.PASS;
     }
 }
