86c577912f34346196f2687e2af4ac364d750b33
StupPlayer <gabrielh.oliveira222@gmail.com>
2021-02-01 11:12:27 -0300

    Avoid inserting items in selected slot when carrying stuff

--- src/main/java/me/steven/carrier/HolderInteractCallback.java
+++ src/main/java/me/steven/carrier/HolderInteractCallback.java
@@ -19,13 +19,14 @@ public class HolderInteractCallback {
 
     private HolderInteractCallback() {
     }
-    public ActionResult interact(PlayerEntity player, World world, Hand hand,BlockPos pos, Direction hitDirection, boolean canPickup) {
+
+    public ActionResult interact(PlayerEntity player, World world, Hand hand, BlockPos pos, Direction hitDirection, boolean canPickup) {
         if (hand == Hand.OFF_HAND) return ActionResult.PASS;
         if (!world.canPlayerModifyAt(player, pos)) return ActionResult.PASS;
         Block block = world.getBlockState(pos).getBlock();
         CarrierComponent carrier = Carrier.HOLDER.get(player);
         CarryingData carrying = carrier.getCarryingData();
-        if (canPickup && carrying == null  && CarriableRegistry.INSTANCE.contains(block)) {
+        if (canPickup && carrying == null && CarriableRegistry.INSTANCE.contains(block)) {
             if (world.isClient && !Carrier.canCarry(Registry.BLOCK.getId(block))) return ActionResult.CONSUME;
             Carriable<?> carriable = CarriableRegistry.INSTANCE.get(block);
             if (world.canPlayerModifyAt(player, pos) && carriable != null && Carrier.canCarry(Registry.BLOCK.getId(block))) {
@@ -50,7 +51,7 @@ public class HolderInteractCallback {
         BlockPos pos = entity.getBlockPos();
         CarrierComponent carrier = Carrier.HOLDER.get(player);
         CarryingData carrying = carrier.getCarryingData();
-        if (canPickup && carrying == null  && CarriableRegistry.INSTANCE.contains(entity.getType())) {
+        if (canPickup && carrying == null && CarriableRegistry.INSTANCE.contains(entity.getType())) {
             if (world.isClient && !Carrier.canCarry(Registry.ENTITY_TYPE.getId(entity.getType()))) return ActionResult.CONSUME;
             Carriable<?> carriable = CarriableRegistry.INSTANCE.get(entity.getType());
             if (world.canPlayerModifyAt(player, pos) && carriable != null && Carrier.canCarry(Registry.ENTITY_TYPE.getId(entity.getType()))) {
@@ -66,16 +67,19 @@ public class HolderInteractCallback {
         }
         return ActionResult.PASS;
     }
+
     public ActionResult interact(PlayerEntity player, World world, Hand hand, BlockHitResult hitResult) {
-        return interact(player,world,hand,hitResult.getBlockPos(),hitResult.getSide(), !Carrier.CONFIG.doGlovesExist()
-                && ((CarrierPlayerExtension) player).canCarry()  && player.getStackInHand(hand).isEmpty());
+        return interact(player, world, hand, hitResult.getBlockPos(), hitResult.getSide(), !Carrier.CONFIG.doGlovesExist()
+                && ((CarrierPlayerExtension) player).canCarry() && player.getStackInHand(hand).isEmpty());
     }
+
     public ActionResult interact(PlayerEntity player, World world, Hand hand, Entity entity) {
         return interact(player, world, hand, entity, !Carrier.CONFIG.doGlovesExist()
                 && ((CarrierPlayerExtension) player).canCarry()
                 && player.getStackInHand(hand).isEmpty());
     }
+
     public ActionResult interact(ItemUsageContext context) {
-        return interact(context.getPlayer(),context.getWorld(),context.getHand(),context.getBlockPos(),context.getSide(), true);
+        return interact(context.getPlayer(), context.getWorld(), context.getHand(), context.getBlockPos(), context.getSide(), true);
     }
 }
--- src/main/java/me/steven/carrier/mixin/MixinPlayerInventory.java
+++ src/main/java/me/steven/carrier/mixin/MixinPlayerInventory.java
@@ -4,21 +4,37 @@ import me.steven.carrier.Carrier;
 import me.steven.carrier.api.CarrierComponent;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.player.PlayerInventory;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.util.collection.DefaultedList;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(PlayerInventory.class)
 public class MixinPlayerInventory {
 
+    private static final ItemStack NON_EMPTY_STACK = new ItemStack(Items.STONE);
+
     @Shadow @Final public PlayerEntity player;
+
+    @Shadow public int selectedSlot;
 
     @Inject(method = "scrollInHotbar", at = @At("HEAD"), cancellable = true)
     private void carrier_cancelSelectedSlotChange(double scrollAmount, CallbackInfo ci) {
         CarrierComponent carrier = Carrier.HOLDER.get(player);
         if (carrier.getCarryingData() != null) ci.cancel();
+    }
+
+    @Redirect(method = "getEmptySlot", at = @At(value = "INVOKE", target = "Lnet/minecraft/util/collection/DefaultedList;get(I)Ljava/lang/Object;"))
+    private Object carrier_ignoreSlot(DefaultedList<ItemStack> defaultedList, int index) {
+        if (index == selectedSlot && Carrier.HOLDER.get(player).getCarryingData() != null)
+            return NON_EMPTY_STACK;
+        return defaultedList.get(index);
     }
 }
