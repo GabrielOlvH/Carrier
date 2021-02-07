0190e860b5b2dde30fd58e7e6c4ede07a00a47e9
StupPlayer <gabrielh.oliveira222@gmail.com>
2021-02-07 12:06:52 -0300

    Don't allow swimming or fall flying while carrying

---
+++ src/main/java/me/steven/carrier/mixin/MixinPlayerEntity.java
@@ -0,0 +1,40 @@
+package me.steven.carrier.mixin;
+
+import me.steven.carrier.Carrier;
+import me.steven.carrier.api.CarrierComponent;
+import me.steven.carrier.api.CarryingData;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(PlayerEntity.class)
+public abstract class MixinPlayerEntity extends LivingEntity  {
+    protected MixinPlayerEntity(EntityType<? extends LivingEntity> entityType, World world) {
+        super(entityType, world);
+    }
+
+    @Inject(method = "updateSwimming", at = @At("HEAD"), cancellable = true)
+    private void a(CallbackInfo ci) {
+        CarrierComponent carrier = Carrier.HOLDER.get(this);
+        CarryingData carrying = carrier.getCarryingData();
+        if (carrying != null) {
+            setSwimming(false);
+            ci.cancel();
+        }
+    }
+
+    @Inject(method = "checkFallFlying", at = @At("HEAD"), cancellable = true)
+    private void b(CallbackInfoReturnable<Boolean> cir) {
+        CarrierComponent carrier = Carrier.HOLDER.get(this);
+        CarryingData carrying = carrier.getCarryingData();
+        if (carrying != null) {
+            cir.setReturnValue(false);
+        }
+    }
+}
