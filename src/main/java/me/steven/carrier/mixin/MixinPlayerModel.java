d28fbfa871ce409fcaab50b26826db57d1db7da8
StupPlayer <gabrielh.oliveira222@gmail.com>
2021-02-07 11:41:59 -0300

    Fix sleeve animation when carrying (closes #11)

--- src/main/java/me/steven/carrier/mixin/MixinPlayerModel.java
+++ src/main/java/me/steven/carrier/mixin/MixinPlayerModel.java
@@ -2,17 +2,24 @@ package me.steven.carrier.mixin;
 
 import me.steven.carrier.Carrier;
 import me.steven.carrier.api.CarrierComponent;
+import net.minecraft.client.model.ModelPart;
 import net.minecraft.client.render.entity.model.BipedEntityModel;
 import net.minecraft.client.render.entity.model.PlayerEntityModel;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(PlayerEntityModel.class)
 public class MixinPlayerModel extends BipedEntityModel<PlayerEntity> {
+
+    @Shadow @Final public ModelPart rightSleeve;
+
+    @Shadow @Final public ModelPart leftSleeve;
 
     public MixinPlayerModel(float scale) {
         super(scale);
@@ -28,6 +35,8 @@ public class MixinPlayerModel extends BipedEntityModel<PlayerEntity> {
             if (player.isSneaking()) pitch = 0.8f;
             rightArm.pitch = -pitch;
             leftArm.pitch = -pitch;
+            rightSleeve.pitch -= pitch;
+            leftSleeve.pitch -= pitch;
         }
     }
 }
