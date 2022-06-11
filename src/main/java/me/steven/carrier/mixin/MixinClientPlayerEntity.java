0be67ec2d6818fe2981166ecadbb35cbb478a451
vanjmali <contact@vanjmali.com>
2022-06-12 00:06:41 +0200

    Update to 1.19

--- src/main/java/me/steven/carrier/mixin/MixinClientPlayerEntity.java
+++ src/main/java/me/steven/carrier/mixin/MixinClientPlayerEntity.java
@@ -3,9 +3,11 @@ package me.steven.carrier.mixin;
 import io.netty.buffer.Unpooled;
 import me.steven.carrier.Carrier;
 import me.steven.carrier.api.CarrierPlayerExtension;
-import net.fabricmc.fabric.api.network.ClientSidePacketRegistry;
+
+import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
 import net.minecraft.client.network.ClientPlayerEntity;
 import net.minecraft.network.PacketByteBuf;
+import net.minecraft.network.listener.ClientPlayPacketListener;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -22,7 +24,7 @@ public class MixinClientPlayerEntity implements CarrierPlayerExtension {
             lastPressed = pressed;
             PacketByteBuf buf = new PacketByteBuf(Unpooled.buffer());
             buf.writeBoolean(pressed);
-            ClientSidePacketRegistry.INSTANCE.sendToServer(Carrier.SET_CAN_CARRY_PACKET, buf);
+            ClientPlayNetworking.send(Carrier.SET_CAN_CARRY_PACKET, buf);
         }
     }
 
